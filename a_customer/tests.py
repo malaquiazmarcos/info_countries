8027d8906f847b953c93278b134044badcf0c05f
Marcos Malaquias <malaquiazmarcos@gmail.com>
2025-03-26 10:25:38 -0300

    commit debug

--- a_customer/tests.py
+++ a_customer/tests.py
@@ -1,21 +1,7 @@
 from django.test import TestCase
 
 
-num = 2
-C = 10 
-cont = 0
-stop = True
 
-while stop:
-    
-    result = num / C
-    cont += 1
-    num = result
 
-    if result >= 0 and result < 1:
-        print(f'El numero tiene {cont} digitos')
-        stop = False
-
-        
 
 
